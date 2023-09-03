d6337351e8d1ac46e146853615a854b4e901ab30
DongOk <hcan159@naver.com>
2023-09-03 23:37:47 +0900

    @add tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <h1></h1></body>
     </html>
   )
 }
