169c5ce50d65ac173817067e7f6c5e986881243e
DongOk <hcan159@naver.com>
2023-09-19 23:44:58 +0900

    @add button Css_2

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -11,6 +11,12 @@ const buttonVariants = cva('active:scale-95 inline-flex items center justify-cen
             default: 'bg-slate-900 text-white hover:bg-slate-800 dark:bg-slate-200 dark:text-slate-900 dark:hover:bg-slate-100',
             outline: 'bg-slate-900 text-white hover:bg-slate-200 dark:text-slate-900 dark:hover:bg-slate-100 border border-slate-200 hover:bg-slate-100 dark:border-slate-700',
             ghost: 'bg-transparent hover:bg-slate-100 gark:hover:bg-slate-800 dark:text-slate-400 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
+            link: 'bg-transparent dark:bg-transparnt underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent',
+        },
+        size:{
+            default:'h-10 py-2 px-2',
+            sm:'h-9 px-2 rounded-md',
+            lg:'h-11 px-8 rounded-md',
         }
     }
 }
