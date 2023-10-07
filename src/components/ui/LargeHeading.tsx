674a0cf8fab3034bcbc9fc8131bb425a83195a07
DongOk <hcan159@naver.com>
2023-10-07 23:31:31 +0900

    @remove inputData

--- src/components/ui/LargeHeading.tsx
+++ src/components/ui/LargeHeading.tsx
@@ -4,18 +4,18 @@ import { cn } from '../lib/utils';
 import { cva, VariantProps} from 'class-variance-authority';
 
 const headingVariants = cva('text-black drak:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter',
-    {
-        variant:{
-            size:{
-                default:'text-4ml md:text-5xl lg:text-6xl',
-                lg:'text-5xl md:text-6xl lg:text-7xl',
-                sm:'text-2xl md:text-3xl lg:text-4xl'
-            }
-        },
-        defaultVariants: {
-            size: 'default',
-        },
-    }
+    // {
+    //     variant:{
+    //         size:{
+    //             default:'text-4ml md:text-5xl lg:text-6xl',
+    //             lg:'text-5xl md:text-6xl lg:text-7xl',
+    //             sm:'text-2xl md:text-3xl lg:text-4xl'
+    //         }
+    //     },
+    //     defaultVariants: {
+    //         size: 'default',
+    //     },
+    // }
 );
 
 interface LargeHeadingProps
