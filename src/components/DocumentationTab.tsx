d2694f56c3d7bfb92efed27cd96baff9bf892ab5
DongOk <hcan159@naver.com>
2023-10-11 15:53:14 +0900

    @add CodeFile for Document

--- src/components/DocumentationTab.tsx
+++ src/components/DocumentationTab.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
-import { Tabs, TabsList, TabsTrigger } from "./ui/Tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/Tabs";
+import SimpleBar from 'simplebar-react'
+import Code from "./Code";
 
 const DocumentationTabs: FC = ()=>{
     return <Tabs defaultValue="nodejs">
@@ -7,6 +9,11 @@ const DocumentationTabs: FC = ()=>{
             <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
             <TabsTrigger value="python">Python</TabsTrigger>
         </TabsList>
+        <TabsContent value="nodejs">
+            {/* <SimpleBar></SimpleBar> */}
+            <Code/> 
+        </TabsContent>
+        <TabsContent value="python"></TabsContent>
     </Tabs>
 }
 export default DocumentationTabs;
