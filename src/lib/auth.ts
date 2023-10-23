be78224cf242ef04e022d67c35c82a6961d42e48
DongOk <hcan159@naver.com>
2023-10-23 21:29:52 +0900

    @copy .env file
    example code from youtube

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from 'next-auth';
 import db from '@/lib/db';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
