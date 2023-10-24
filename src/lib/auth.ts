f6664c4398023a36ee77de39dbf01061a78ac997
DongOk <hcan159@naver.com>
2023-10-24 14:15:55 +0900

    @add api setting

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -29,13 +29,17 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ token, session }) {
+   session({ token, session }) {
       if (token) {
         session.user.id = token.id;
         session.user.name = token.name;
         session.user.email = token.email;
-        session.user.image = token.image;
+        session.user.image = token.picture;
       }
+      return session;
     },
+    async jwt({token, user}) {
+      const dbUser = await db.
+    }
   },
 };
